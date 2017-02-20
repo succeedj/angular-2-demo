387fbeb952f7966a78bd576e16612738ad87ea24
Succeed Jabangwe <succeedjabangwe@Succeeds-iMac.home>
2017-02-19 23:37:23 -0600

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HousesListComponent } from './components/houses/houses-list/houses-list.component';
+import { HouseDetailsComponent } from './components/houses/house-details/house-details.component';
+import { AddHouseComponent } from './components/houses/add-house/add-house.component';
+import { HouseSnapshotComponent } from './components/houses/house-snapshot/house-snapshot.component';
+import {HouseService} from "./services/house.service";
+import {RouterModule} from "@angular/router";
+import {ROUTES} from "./routes";
+import {HousesListResolverService} from "./resolvers/houses-list-resolver.service";
+import { AddRealtorComponent } from './components/houses/add-realtor/add-realtor.component';
+import { RealtorListComponent } from './components/houses/realtor-list/realtor-list.component';
+
+import {TOASTR_TOKEN, Toastr} from './common/toastr.service';
+import { ModalDirective } from './common/modal.directive';
+import { JQ_TOKEN } from './common/jQuery.service';
+import { AppModalComponent } from './common/app-modal/app-modal.component';
+
+declare let toastr: Toastr;
+declare let jQuery: Object;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HousesListComponent,
+    HouseDetailsComponent,
+    AddHouseComponent,
+    HouseSnapshotComponent,
+    AddRealtorComponent,
+    RealtorListComponent,
+    ModalDirective,
+    AppModalComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [
+    HouseService,
+    HousesListResolverService,
+    { provide: TOASTR_TOKEN, useValue: toastr},
+    { provide: JQ_TOKEN, useValue: jQuery },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
